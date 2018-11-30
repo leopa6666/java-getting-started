6b248a99aa34b5d041505b0f97244950e0db50a0
leopa6666 <40862417+leopa6666@users.noreply.github.com>
2018-11-30 23:52:42 +0900

    add sql

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -62,7 +62,7 @@ public class Main {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
       stmt.executeUpdate("INSERT INTO ticks VALUES (now(),'test')");
-      ResultSet rs = stmt.executeQuery("SELECT tick, FROM ticks");
+      ResultSet rs = stmt.executeQuery("SELECT tick,description FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
